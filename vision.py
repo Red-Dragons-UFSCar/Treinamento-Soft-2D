247f2b223f27e313b11be789250296d5fd785c35
beatrizfsrusso <beatrizfsrusso@yahoo.com.br>
2025-03-23 20:35:42 -0300

    Vision
    Codigo da classe Vision

---
+++ vision.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+class Vision:
+
+    def getPoseRobot(self):
+       x_robot = np.random.uniform(0,150)
+       y_robot = np.random.uniform(0,130)
+       theta = np.random.uniform (-np.pi, np.pi)
+       return (x_robot, y_robot, theta)
+    
+    def getPoseBall(self):
+        x_ball = np.random.uniform(0,150)
+        y_ball = np.random.uniform(0,130)
+        return (x_ball, y_ball)
